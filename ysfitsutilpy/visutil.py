407ca377ebaec97b349f132d83074e51a8708116
ysBach <dbstn95@gmail.com>
2019-07-03 16:51:04 +0900

    pep8

--- ysfitsutilpy/visutil.py
+++ ysfitsutilpy/visutil.py
@@ -1,12 +1,16 @@
-from astropy.visualization import ZScaleInterval, ImageNormalize
+from astropy.visualization import ImageNormalize, LinearStretch, ZScaleInterval
 
 __all__ = ["znorm", "zimshow"]
 
 
-def znorm(image):
-    return ImageNormalize(image, interval=ZScaleInterval())
+def znorm(image, stretch=LinearStretch(), **kwargs):
+    return ImageNormalize(image,
+                          interval=ZScaleInterval(**kwargs),
+                          stretch=stretch)
 
 
-def zimshow(ax, image, **kwargs):
-    return ax.imshow(image, norm=znorm(image), origin='lower', **kwargs)
-
+def zimshow(ax, image, stretch=LinearStretch(), cmap=None, **kwargs):
+    return ax.imshow(image,
+                     norm=znorm(image, stretch=stretch, **kwargs),
+                     origin='lower',
+                     cmap=cmap)
