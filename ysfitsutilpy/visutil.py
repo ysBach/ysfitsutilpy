7e62159ba982daf252eccf83300234c0ba90809a
ysBach <dbstn95@gmail.com>
2019-11-04 16:48:59 +0900

    deprecate visutil over ysvisutilpy

--- ysfitsutilpy/visutil.py
+++ ysfitsutilpy/visutil.py
@@ -6,18 +6,23 @@ from astropy.visualization import (ImageNormalize, LinearStretch,
 from matplotlib.ticker import (FormatStrFormatter, LogFormatterSciNotation,
                                LogLocator, MultipleLocator, NullFormatter)
 
+
 __all__ = ["znorm", "zimshow", "norm_imshow", "colorbaring",
            "mplticker", "linticker", "logticker", "logxticker", "logyticker",
            "linearticker"]
 
 
 def znorm(image, stretch=LinearStretch(), **kwargs):
+    warn("Using znorm in ysfitsutilpy is deprecated. "
+         + "Please use ysvisutilpy.")
     return ImageNormalize(image,
                           interval=ZScaleInterval(**kwargs),
                           stretch=stretch)
 
 
 def zimshow(ax, image, stretch=LinearStretch(), cmap=None, **kwargs):
+    warn("Using zimshow in ysfitsutilpy is deprecated. "
+         + "Please use ysvisutilpy.")
     im = ax.imshow(image,
                    norm=znorm(image, stretch=stretch, **kwargs),
                    origin='lower',
@@ -30,6 +35,8 @@ def norm_imshow(ax, data, stretch='linear', power=1.0, asinh_a=0.1,
                 percent=None, clip=True, log_a=1000, **kwargs):
     """ Do normalization and do imshow
     """
+    warn("Using norm_imshow in ysfitsutilpy is deprecated. "
+         + "Please use ysvisutilpy.")
     norm = simple_norm(data, stretch=stretch, power=power, asinh_a=asinh_a,
                        min_cut=min_cut, max_cut=max_cut,
                        min_percent=min_percent, max_percent=max_percent,
@@ -40,66 +47,12 @@ def norm_imshow(ax, data, stretch='linear', power=1.0, asinh_a=0.1,
 
 def colorbaring(fig, ax, im, fmt="%.0f", orientation='horizontal',
                 formatter=FormatStrFormatter, **kwargs):
+    warn("Using colorbaring in ysfitsutilpy is deprecated. "
+         + "Please use ysvisutilpy.")
     cb = fig.colorbar(im, ax=ax, orientation=orientation,
                       format=FormatStrFormatter(fmt), **kwargs)
 
     return cb
-
-
-def linearticker(ax_list, xmajlocs, xminlocs, ymajlocs, yminlocs,
-                 xmajfmts, ymajfmts,
-                 xmajlocators=MultipleLocator,
-                 xminlocators=MultipleLocator,
-                 ymajlocators=MultipleLocator,
-                 yminlocators=MultipleLocator,
-                 xmajformatters=FormatStrFormatter,
-                 ymajformatters=FormatStrFormatter,
-                 majgridkw=dict(ls='-', alpha=0.8),
-                 mingridkw=dict(ls=':', alpha=0.5)):
-    warn("Use mplticker instead of linearticker.")
-    _ax_list = np.atleast_1d(ax_list)
-    n_axis = len(_ax_list)
-    xmajloc = np.atleast_1d(xmajlocs)
-    xminloc = np.atleast_1d(xminlocs)
-    ymajloc = np.atleast_1d(ymajlocs)
-    yminloc = np.atleast_1d(yminlocs)
-    xmajfmt = np.atleast_1d(xmajfmts)
-    ymajfmt = np.atleast_1d(ymajfmts)
-    if xmajloc.shape[0] != n_axis:
-        xmajloc = np.repeat(xmajloc, n_axis)
-    if xminloc.shape[0] != n_axis:
-        xminloc = np.repeat(xminloc, n_axis)
-    if ymajloc.shape[0] != n_axis:
-        ymajloc = np.repeat(ymajloc, n_axis)
-    if yminloc.shape[0] != n_axis:
-        yminloc = np.repeat(yminloc, n_axis)
-    if xmajfmt.shape[0] != n_axis:
-        xmajfmt = np.repeat(xmajfmt, n_axis)
-    if ymajfmt.shape[0] != n_axis:
-        ymajfmt = np.repeat(ymajfmt, n_axis)
-
-    if not isinstance(xmajlocators, (tuple, list, np.ndarray)):
-        xmajlocators = [xmajlocators] * n_axis
-    if not isinstance(xminlocators, (tuple, list, np.ndarray)):
-        xminlocators = [xminlocators] * n_axis
-    if not isinstance(ymajlocators, (tuple, list, np.ndarray)):
-        ymajlocators = [ymajlocators] * n_axis
-    if not isinstance(yminlocators, (tuple, list, np.ndarray)):
-        yminlocators = [yminlocators] * n_axis
-    if not isinstance(xmajformatters, (tuple, list, np.ndarray)):
-        xmajformatters = [xmajformatters] * n_axis
-    if not isinstance(ymajformatters, (tuple, list, np.ndarray)):
-        ymajformatters = [ymajformatters] * n_axis
-
-    for i, aa in enumerate(_ax_list):
-        aa.xaxis.set_major_locator(xmajlocators[i](xmajloc[i]))
-        aa.yaxis.set_major_locator(ymajlocators[i](ymajloc[i]))
-        aa.xaxis.set_minor_locator(xminlocators[i](xminloc[i]))
-        aa.yaxis.set_minor_locator(yminlocators[i](yminloc[i]))
-        aa.xaxis.set_major_formatter(xmajformatters[i](xmajfmt[i]))
-        aa.yaxis.set_major_formatter(ymajformatters[i](ymajfmt[i]))
-        aa.grid(which='major', **majgridkw)
-        aa.grid(which='minor', **mingridkw)
 
 
 def mplticker(ax_list,
@@ -180,6 +133,9 @@ def mplticker(ax_list,
         default arguments.
 
     '''
+    warn("Using mplticker in ysfitsutilpy is deprecated. "
+         + "Please use ysvisutilpy.")
+
     def _check(obj, name, n):
         arr = np.atleast_1d(obj)
         n_arr = arr.shape[0]
@@ -311,6 +267,8 @@ def linticker(ax_list,
               xmingridkws=dict(ls=':', alpha=0.5),
               ymajgridkws=dict(ls='-', alpha=0.5),
               ymingridkws=dict(ls=':', alpha=0.5)):
+    warn("Using linticker in ysfitsutilpy is deprecated. "
+         + "Please use ysvisutilpy.")
     mplticker(**locals())
 
 
@@ -331,6 +289,8 @@ def logticker(ax_list,
               xmingridkws=dict(ls=':', alpha=0.5),
               ymajgridkws=dict(ls='-', alpha=0.5),
               ymingridkws=dict(ls=':', alpha=0.5)):
+    warn("Using logticker in ysfitsutilpy is deprecated. "
+         + "Please use ysvisutilpy.")
     mplticker(**locals())
 
 
@@ -351,6 +311,8 @@ def logxticker(ax_list,
                xmingridkws=dict(ls=':', alpha=0.5),
                ymajgridkws=dict(ls='-', alpha=0.5),
                ymingridkws=dict(ls=':', alpha=0.5)):
+    warn("Using logxticker in ysfitsutilpy is deprecated. "
+         + "Please use ysvisutilpy.")
     mplticker(**locals())
 
 
@@ -371,4 +333,64 @@ def logyticker(ax_list,
                xmingridkws=dict(ls=':', alpha=0.5),
                ymajgridkws=dict(ls='-', alpha=0.5),
                ymingridkws=dict(ls=':', alpha=0.5)):
+    warn("Using logyticker in ysfitsutilpy is deprecated. "
+         + "Please use ysvisutilpy.")
     mplticker(**locals())
+
+
+def linearticker(ax_list, xmajlocs, xminlocs, ymajlocs, yminlocs,
+                 xmajfmts, ymajfmts,
+                 xmajlocators=MultipleLocator,
+                 xminlocators=MultipleLocator,
+                 ymajlocators=MultipleLocator,
+                 yminlocators=MultipleLocator,
+                 xmajformatters=FormatStrFormatter,
+                 ymajformatters=FormatStrFormatter,
+                 majgridkw=dict(ls='-', alpha=0.8),
+                 mingridkw=dict(ls=':', alpha=0.5)):
+    warn("Using linearticker in ysfitsutilpy is deprecated. "
+         + "Please use ysvisutilpy.")
+    warn("Use mplticker instead of linearticker.")
+    _ax_list = np.atleast_1d(ax_list)
+    n_axis = len(_ax_list)
+    xmajloc = np.atleast_1d(xmajlocs)
+    xminloc = np.atleast_1d(xminlocs)
+    ymajloc = np.atleast_1d(ymajlocs)
+    yminloc = np.atleast_1d(yminlocs)
+    xmajfmt = np.atleast_1d(xmajfmts)
+    ymajfmt = np.atleast_1d(ymajfmts)
+    if xmajloc.shape[0] != n_axis:
+        xmajloc = np.repeat(xmajloc, n_axis)
+    if xminloc.shape[0] != n_axis:
+        xminloc = np.repeat(xminloc, n_axis)
+    if ymajloc.shape[0] != n_axis:
+        ymajloc = np.repeat(ymajloc, n_axis)
+    if yminloc.shape[0] != n_axis:
+        yminloc = np.repeat(yminloc, n_axis)
+    if xmajfmt.shape[0] != n_axis:
+        xmajfmt = np.repeat(xmajfmt, n_axis)
+    if ymajfmt.shape[0] != n_axis:
+        ymajfmt = np.repeat(ymajfmt, n_axis)
+
+    if not isinstance(xmajlocators, (tuple, list, np.ndarray)):
+        xmajlocators = [xmajlocators] * n_axis
+    if not isinstance(xminlocators, (tuple, list, np.ndarray)):
+        xminlocators = [xminlocators] * n_axis
+    if not isinstance(ymajlocators, (tuple, list, np.ndarray)):
+        ymajlocators = [ymajlocators] * n_axis
+    if not isinstance(yminlocators, (tuple, list, np.ndarray)):
+        yminlocators = [yminlocators] * n_axis
+    if not isinstance(xmajformatters, (tuple, list, np.ndarray)):
+        xmajformatters = [xmajformatters] * n_axis
+    if not isinstance(ymajformatters, (tuple, list, np.ndarray)):
+        ymajformatters = [ymajformatters] * n_axis
+
+    for i, aa in enumerate(_ax_list):
+        aa.xaxis.set_major_locator(xmajlocators[i](xmajloc[i]))
+        aa.yaxis.set_major_locator(ymajlocators[i](ymajloc[i]))
+        aa.xaxis.set_minor_locator(xminlocators[i](xminloc[i]))
+        aa.yaxis.set_minor_locator(yminlocators[i](yminloc[i]))
+        aa.xaxis.set_major_formatter(xmajformatters[i](xmajfmt[i]))
+        aa.yaxis.set_major_formatter(ymajformatters[i](ymajfmt[i]))
+        aa.grid(which='major', **majgridkw)
+        aa.grid(which='minor', **mingridkw)
