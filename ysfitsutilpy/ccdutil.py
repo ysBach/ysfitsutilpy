a4213612010cc2711d0a03318b9b51b33fefdf41
ysBach <dbstn95@gmail.com>
2020-06-12 13:58:23 +0900

    imcopy to add LTMi_j to header for consistency

--- ysfitsutilpy/ccdutil.py
+++ ysfitsutilpy/ccdutil.py
@@ -189,16 +189,17 @@ def trim_ccd(ccd, fits_section=None, add_keyword=True, verbose=False):
         ltv1 = 0.
         ltv2 = 0.
 
-    try:  # if LTV and LTM exists already
-        trimmed_ccd.header["LTV1"] += ltv1
-        trimmed_ccd.header["LTV2"] += ltv2
-        trimmed_ccd.header["LTM1_1"] *= 1.
-        trimmed_ccd.header["LTM2_2"] *= 1.
-    except KeyError:
-        trimmed_ccd.header["LTV1"] = ltv1
-        trimmed_ccd.header["LTV2"] = ltv2
-        trimmed_ccd.header["LTM1_1"] = 1.
-        trimmed_ccd.header["LTM2_2"] = 1.
+    for k, v in zip(["LTV1", "LTV2"], [ltv1, ltv2]):
+        try:  # if LTV exists already
+            trimmed_ccd.header[k] += v
+        except KeyError:
+            trimmed_ccd.header[k] = v
+
+    for k in ["LTM1_1", "LTM1_2", "LTM2_1", "LTM2_2"]:
+        try:  # if LTM exists already
+            trimmed_ccd.header[k] *= 1.
+        except KeyError:
+            trimmed_ccd.header[k] = 1.
 
     add_to_header(trimmed_ccd.header, 'h', trim_str, t_ref=_t, verbose=verbose)
     return trimmed_ccd
